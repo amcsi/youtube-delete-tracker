49bae71acae30fa52da41ef278e4a83b1961f496
Attila Szeremi <attila@szeremi.org>
2019-01-11 18:40:53 +0100

    Add option to scan all tracked channels in ScanChannelCommand

--- app/Console/Commands/ScanChannelCommand.php
+++ app/Console/Commands/ScanChannelCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Channel;
 use App\Channel\ChannelAdderAndScanner;
 use App\Debug\Profiling;
 use Illuminate\Console\Command;
@@ -11,7 +12,7 @@ class ScanChannelCommand extends Command
 {
     private const COMMAND = 'scan-channel';
 
-    protected $signature = self::COMMAND . ' {channelId}';
+    protected $signature = self::COMMAND . ' {--all : Scan all tracked playlists} {channelId? : Scan the given youtube channel ID}';
     protected $description = 'Scans all playlists of a channel.';
 
     private $channelAdderAndScanner;
@@ -24,11 +25,24 @@ class ScanChannelCommand extends Command
 
     public function handle()
     {
-        $stopwatchEvent = (new Stopwatch())->start('scan-channel');
-        $this->output->text('Started scanning channel.');
+        $stopwatchEvent = (new Stopwatch())->start('scan-channels');
+        $this->output->text('Started scanning channels.');
 
-        $this->channelAdderAndScanner->addAndScan($this->argument('channelId'));
+        if ($this->option('all')) {
+            $youtubeChannelIds = Channel::whereTrack(true)->get(['external_channel_id'])->pluck('external_channel_id');
+        } else {
+            $youtubeChannelIds = [$this->argument('channelId')];
+        }
 
-        $this->output->text('Done scanning channel. ' . Profiling::stopwatchToHuman($stopwatchEvent));
+        foreach ($youtubeChannelIds as $youtubeChannelId) {
+            $channelEvent = (new Stopwatch())->start('scan-channel');
+            $this->output->text("Started scanning channel $youtubeChannelId.");
+
+            $this->channelAdderAndScanner->addAndScan($youtubeChannelId);
+
+            $this->output->text('Done scanning channel. ' . Profiling::stopwatchToHuman($channelEvent));
+        }
+
+        $this->output->text('Done scanning channels. ' . Profiling::stopwatchToHuman($stopwatchEvent));
     }
 }
