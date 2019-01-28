63f1a7e60325870949831c7484b34e230ca29dfd
Attila Szeremi <attila@szeremi.org>
2019-01-28 14:19:51 +0100

    Use imperative Jenkinsfile; add deploy step

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,15 +1,20 @@
-pipeline {
-    agent {
-        dockerfile {
-            args '--network szeremi -e DB_HOST=mysql -e APP_ENV=testing'
+node {
+    checkout scm
+
+    def customImage
+
+    stage('Build image') {
+        customImage = docker.build("my-image:${env.BUILD_ID}")
+    }
+
+    stage('Run tests') {
+        customImage.inside('--network szeremi -e DB_HOST=mysql -e APP_ENV=testing') {
+            sh 'cd /var/www && php artisan migrate:fresh && vendor/bin/phpunit'
         }
     }
 
-    stages {
-        stage('Run tests') {
-            steps {
-                sh 'cd /var/www && php artisan migrate:fresh && vendor/bin/phpunit'
-            }
-        }
+    stage('Deploy') {
+        // Re-tag the image, because the deployment server is the same as the Jenkins server.
+        sh "docker tag ${customImage.id} amcsi/youtube-delete-tracker"
     }
 }
