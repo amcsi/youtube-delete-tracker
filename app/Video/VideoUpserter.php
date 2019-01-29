fc5b9003f4839f773c54d21bc6e4866bfd90d110
Attila Szeremi <attila@szeremi.org>
2019-01-29 18:24:19 +0100

    Mark deleted videos during playlist scan

--- app/Video/VideoUpserter.php
+++ app/Video/VideoUpserter.php
@@ -4,28 +4,41 @@ declare(strict_types=1);
 namespace App\Video;
 
 use App\Video;
-use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
 
 class VideoUpserter
 {
     public static function upsertFromPlaylistItem(\Google_Service_YouTube_PlaylistItem $youtubePlaylistItem): ?int
     {
         $snippet = $youtubePlaylistItem->getSnippet();
-        $videoId = $snippet->getResourceId()->videoId;
+        $values = [
+            'title' => $snippet->title,
+            'known_deleted_at' => null, // In case the video had previously been marked deleted upstream.
+        ];
+        $externalVideoId = $snippet->getResourceId()->videoId;
         if (!$snippet->getThumbnails()) {
-            // This is unexpected; deleted videos normally just shouldn't show up. Log it, and do not upsert here.
-            $message = sprintf('Video with a youtube ID of %s got a title of "Deleted video"', $videoId);
-            Log::warning($message);
-            return null;
+            // The video is deleted or private.
+
+            $video = Video::where(['external_video_id' => $externalVideoId])->first();
+            if (!$video) {
+                // We don't even have a video like that locally; just don't bother with this deleted video.
+                return null;
+            }
+
+            // Mark the video as deleted.
+
+            if (!$video->known_deleted_at) {
+                // Only set the deletion date if it's not set already.
+                $video->known_deleted_at = Carbon::now();
+            }
+
+            return $video->id;
         }
         $video = Video::updateOrCreate(
             [
-                'external_video_id' => $snippet->getResourceId()->videoId,
+                'external_video_id' => $externalVideoId,
             ],
-            [
-                'title' => $snippet->title,
-                'known_deleted_at' => null, // In case the video had previously been marked deleted upstream.
-            ]
+            $values
         );
         return $video->id;
     }
